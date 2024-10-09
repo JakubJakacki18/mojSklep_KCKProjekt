f7fe784f526be78d1450ad2431a10c27b490fcec
Jakub <jakub.jakacki18@gmail.com>
2024-10-09 17:21:21 +0200

    -wprowadzenie wyboru opcji za pomocą klawiszy

--- Views/CLIViews/UserCLIView.cs
+++ Views/CLIViews/UserCLIView.cs
@@ -12,13 +12,29 @@ namespace KCKProjekt.Views.CLIViews
 	{
 		public void showSignIn()
 		{
-			//AnsiConsole.Markup("[underline bold green]Zaloguj się![/]\n");
+			ConsoleKey consoleKey;
+			do {
+				AnsiConsole.Markup("[underline bold green]Wciśnij enter jeśli chcesz przejść do ekranu logowania lub spację jeśli do rejestracji[/]\n");
+				consoleKey = Console.ReadKey().Key;
+			} while (consoleKey != ConsoleKey.Enter && consoleKey != ConsoleKey.Spacebar);
+			
+			switch (consoleKey)
+			{
+				case ConsoleKey.Spacebar:
+					showSignUp();
+					break;
+				case ConsoleKey.Enter:
+					break;
+				default:
+					throw new InvalidOperationException("Coś poszło nie tak");
+			}
+			AnsiConsole.Markup("[underline bold green]Zaloguj się![/]\n");
+			var username = AnsiConsole.Ask<string>("Enter your [green]username[/]:");
+			var password = AnsiConsole.Prompt(
+				new TextPrompt<string>("Enter your [green]password[/]:")
+					.PromptStyle("yellow")
+					.Secret());
 
-			//var username = AnsiConsole.Ask<string>("Enter your [green]username[/]:");
-			//var password = AnsiConsole.Prompt(
-			//	new TextPrompt<string>("Enter your [green]password[/]:")
-			//		.PromptStyle("yellow")
-			//		.Secret());
 
 			var layout = new Layout("Root")
 	.SplitColumns(
@@ -32,7 +48,7 @@ namespace KCKProjekt.Views.CLIViews
 			layout["Left"].Update(
 				new Panel(
 					Align.Center(
-						new Markup("Hello [blue]World![/]"),
+						new Markup("[underline bold green]Zaloguj się![/]\n"),
 						VerticalAlignment.Middle))
 					.Expand());
 
