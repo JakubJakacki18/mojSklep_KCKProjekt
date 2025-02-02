3d4cb0eb032bc26a59f8640c5b8b6915a51ddde4
Jakub Jakacki <jakub.jakacki18@gmail.com>
2025-02-02 12:59:37 +0100

    - dodanie selectora projektu

---
+++ InterfaceSelector/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+class InterfaceSelector
+{
+    static void Main(string[] args)
+    {
+
+
+        string appPath = "";
+        string? line;
+        do
+        {
+            System.Console.WriteLine("0 - console \n1 - wpf");
+            line = Console.ReadLine();
+            switch (line)
+            {
+                case "0":
+                    appPath = @"C:\Users\Jakub\source\repos\KCKProjekt\ConsoleApp\bin\Debug\net8.0\ConsoleApp.exe";
+                    break;
+                case "1":
+                    appPath = @"C:\Users\Jakub\source\repos\KCKProjekt\WpfApp\bin\Debug\net8.0-windows\WpfApp.exe";
+                    break;
+                default:
+                    System.Console.WriteLine("Bład, wpisz jeszcze raz");
+                    break;
+
+            }
+        } while (line != "0" && line != "1");
+
+
+        Process process = new();
+        process.StartInfo.FileName = appPath;
+
+        process.Start();
+    }
+}
