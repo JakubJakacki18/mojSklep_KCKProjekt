9a7b237aaccec3bc93c467f121336d374ee3237d
Jakub <jakub.jakacki18@gmail.com>
2025-02-02 18:31:33 +0100

    - dołączenie działającego launchera

--- InterfaceSelector/Program.cs
+++ InterfaceSelector/Program.cs
@@ -2,35 +2,30 @@
 
 class InterfaceSelector
 {
-    static void Main(string[] args)
-    {
+	static void Main(string[] args)
+	{
+		string appPath = "";
+		string? line;
+		do
+		{
+			System.Console.WriteLine("0 - console \n1 - wpf");
+			line = Console.ReadLine();
+			switch (line)
+			{
+				case "0":
+					appPath = @"C:\Users\Jakub\source\repos\KCKProjekt\ConsoleApp\bin\Debug\net8.0\ConsoleApp.exe";
+					break;
+				case "1":
+					appPath = @"C:\Users\Jakub\source\repos\KCKProjekt\WpfApp\bin\Debug\net8.0-windows8.0\WpfApp.exe";
+					break;
+				default:
+					System.Console.WriteLine("Bład, wpisz jeszcze raz");
+					break;
 
-
-        string appPath = "";
-        string? line;
-        do
-        {
-            System.Console.WriteLine("0 - console \n1 - wpf");
-            line = Console.ReadLine();
-            switch (line)
-            {
-                case "0":
-                    appPath = @"C:\Users\Jakub\source\repos\KCKProjekt\ConsoleApp\bin\Debug\net8.0\ConsoleApp.exe";
-                    break;
-                case "1":
-                    appPath = @"C:\Users\Jakub\source\repos\KCKProjekt\WpfApp\bin\Debug\net8.0-windows\WpfApp.exe";
-                    break;
-                default:
-                    System.Console.WriteLine("Bład, wpisz jeszcze raz");
-                    break;
-
-            }
-        } while (line != "0" && line != "1");
-
-
-        Process process = new();
-        process.StartInfo.FileName = appPath;
-
-        process.Start();
-    }
+			}
+		} while (line != "0" && line != "1");
+		Process process = new();
+		process.StartInfo.FileName = appPath;
+		process.Start();
+	}
 }
